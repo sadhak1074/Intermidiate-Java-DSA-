257521b255b6860e16ec8970871a0103bb704e7a
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-05-14 00:09:08 +0530

    Recursion2

--- recursion1.java
+++ recursion1.java
@@ -1,14 +1,14 @@
 public class recursion1 {
     public static void printNum(int n){
-        if(n==0){
+        if(n==6){
             return;
         }
         System.out.println(n);
-        printNum(n-1);
+        printNum(n+1);
     }
 
     public static void main(String args[]){
-        int n = 5;
+        int n = 1;
         printNum(n);
     
     }
