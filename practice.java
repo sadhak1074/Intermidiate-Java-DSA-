9eb1b43a877ccd087c919aebade6ade9dc1c35a3
sadhak1074 <sadhaks.bcads2021@iujaipur.edu.in>
2023-05-16 21:40:37 +0530

    Fibbnocci series

--- practice.java
+++ practice.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
-public class practice {
-    public static void main(String[] args) {
-        int i = 0;
-        while (i < 10) {
-            System.out.println(i);
-            i++;
+import java.util.*;
+
+public class practice{
+    public static void main(String[] args){
+        int n = 4; 
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
-    
 }
